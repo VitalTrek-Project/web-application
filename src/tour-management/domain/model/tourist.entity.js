18aa286cd743f9711c6cf07ba76001c85be972d7
Miler <mileralexander2003@gmail.com>
2026-05-13 17:53:09 -0500

    feat: add Tourist entity class

---
+++ src/tour-management/domain/model/tourist.entity.js
@@ -0,0 +1,8 @@
+export class Tourist {
+    constructor(id, fullName, email, assignmentStatus) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.assignmentStatus = assignmentStatus;
+    }
+}
