d5fd37cd0ae8b71ba02253dae6b5bc5257f495fa
Miler <mileralexander2003@gmail.com>
2026-05-14 05:40:03 -0500

    feat: add tour management routes for listing, creating, and editing tours

--- src/router.js
+++ src/router.js
@@ -1,5 +1,8 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Home from "./shared/presentation/views/home.vue";
+import TourList from "./tour-management/presentation/views/tour-list.vue";
+import TourForm from "./tour-management/presentation/views/tour-form.vue";
+import TouristsAssignment from "./tour-management/presentation/views/tourists-assigment.vue";
 
 // To import when IAM is implemented,
 // import iamRoutes from "./iam/presentation/iam-routes.js";
@@ -23,6 +26,10 @@ const routes = [
 const routes = [
     { path: '/home',            name: 'home',       component: Home,        meta: { title: 'Home' } },
     { path: '/about',           name: 'about',      component: about,       meta: { title: 'About' } },
+    { path: '/tours', name: 'tour-management-tours', component: TourList, meta: { title: 'Tour' } },
+    { path: '/tours/new', name: 'tour-management-tour-new', component: TourForm, meta: { title: 'New Tour' } },
+    { path: '/tours/:id/edit', name: 'tour-management-tour-edit', component: TourForm, meta: { title: 'Edit Tour' } },
+    { path: '/tourists-assignment', name: 'tour-management-tourists-assignment', component: TouristsAssignment, meta: { title: 'Tourists Assignment' } },
 
     { path: '/',                redirect: '/home' },
     { path: '/:pathMatch(.*)*', name: 'not-found', component: pageNotFound, meta: { title: 'Page Not Found' } }
